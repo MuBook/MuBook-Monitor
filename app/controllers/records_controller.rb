05b252a09c22fd6f8e6febad58934f3488e9f171
Po Chen <pchen@zendesk.com>
2014-02-23 17:25:57 +1100

    Clean init up

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,40 +1,38 @@
 class RecordsController < ApplicationController
   before_action :set_record, only: [:show]
 
-  # GET /records
-  # GET /records.json
   def index
     @records = Record.all
   end
 
-  # GET /records/1
-  # GET /records/1.json
   def show
   end
 
-  # POST /records
-  # POST /records.json
   def create
     @record = Record.new(record_params)
 
     respond_to do |format|
       if @record.save
-        format.html { redirect_to @record, notice: 'Record was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @record }
+        format.html { head: :created }
+        format.json { render json: @record }
       else
-        format.html { render action: 'new' }
-        format.json { render json: @record.errors, status: :unprocessable_entity }
+        Record.create(
+          name: 'Monitor',
+          email: 'system@mubook.me',
+          title: 'Error',
+          message: JSON.pretty_generate(record_params)
+        )
+
+        head: :internal_server_error
       end
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_record
       @record = Record.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def record_params
       params.require(:record).permit(:name, :email, :title, :message)
     end
